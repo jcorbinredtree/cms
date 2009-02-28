abb8f060ef5856a9972c0cfdd8a736088717febb
Joshua T Corbin <jcorbin@redtreesystems.com>
2009-02-27 21:19:10 -0500

    Add CMSDBObject

---
+++ CMSDBObject.php
@@ -0,0 +1,147 @@
+<?php
+/** * CMSPage definition
+ *
+ * PHP version 5
+ *
+ * LICENSE: The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is Red Tree Systems Code.
+ *
+ * The Initial Developer of the Original Code is Red Tree Systems, LLC. All Rights Reserved.
+ *
+ * @category     CMS
+ * @author       Red Tree Systems, LLC <support@redtreesystems.com>
+ * @copyright    2009 Red Tree Systems, LLC
+ * @license      MPL 1.1
+ * @version      2.0
+ */
+
+require_once 'lib/database/DatabaseObject.php';
+require_once 'lib/cms/CMSDBObjectDataTable.php';
+
+/**
+ * Common base class for CMSPage and CMSNode, provides the interface needed by
+ * CMSDBTemplateProvider
+ */
+abstract class CMSDBObject extends DatabaseObject
+{
+    public static $ManualColumns = array(
+        'modified'
+    );
+
+    public static $CustomSQL = array(
+        'get_modified' =>
+            'SELECT modified FROM {table} WHERE {key}=?',
+        'set_modified' =>
+            'UPDATE {table} SET modified=FROM_UNIXTIME(?) WHERE {key}=?'
+    );
+
+    protected $modified=null;
+
+    public $data;
+
+    protected $unserializing=false;
+
+    abstract public function getContent();
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->data = new CMSDBObjectDataTable($this);
+    }
+
+    public function create()
+    {
+        parent::create();
+        $this->modified = null;
+    }
+
+    public function update()
+    {
+        parent::create();
+        $this->setModified();
+    }
+
+    public function delete()
+    {
+        global $database;
+
+        $database->transaction();
+        try {
+            $this->data->clearAll();
+            parent::delete();
+        } catch (Exception $e) {
+            $database->rollback();
+        }
+        $database->commit();
+    }
+
+    public function getModified()
+    {
+        if (! isset($this->modified)) {
+            if (isset($this->id)) {
+                global $database;
+                $sql = $this->meta()->getCustomSQL('get_modified');
+                $sth = $database->executef($sql, $this->id);
+                $r = $sth->fetch(PDF::FETCH_NUM);
+                $this->modified = $r[0];
+            }
+        }
+        return $this->modified;
+    }
+
+    public function setModified($when=null)
+    {
+        if ($this->unserializing) {
+            return;
+        }
+        if (! isset($when)) {
+            $when = time();
+        }
+        assert(is_int($when));
+        if ($when < $this->modified) {
+            throw new InvalidArgumentException("Won't roll back modified time");
+        }
+
+        if (isset($this->id)) {
+            global $database;
+            $meta = $this->meta();
+            $sql = $this->meta()->getCustomSQL('set_modified');
+            $database->executef($sql, $when, $this->id);
+        }
+        $this->modified = $when;
+    }
+
+    public function serialize()
+    {
+        $data = parent::serialize();
+        $data['datatable'] = $this->data->serialize();
+        return $data;
+    }
+
+    public function unserialize($data, $save=true)
+    {
+        $this->unserializing = true;
+        parent::unserialize($data, $save);
+        if (
+            array_key_exists('datatable', $data) &&
+            is_array($data['datatable'])
+        ) {
+            $this->data->unserialize($data['datatable']);
+        }
+        $this->unserializing = false;
+    }
+}
+
+CMSDBObject::$CustomSQL = array_merge(
+    CMSDBObject::$CustomSQL,
+    CMSDBObjectDataTable::$CustomSQL
+);
+
+?>
---
+++ CMSDBObjectDataTable.php
@@ -0,0 +1,297 @@
+<?php
+
+/**
+ * CMSPage definition
+ *
+ * PHP version 5
+ *
+ * LICENSE: The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is Red Tree Systems Code.
+ *
+ * The Initial Developer of the Original Code is Red Tree Systems, LLC. All Rights Reserved.
+ *
+ * @category     CMS
+ * @author       Red Tree Systems, LLC <support@redtreesystems.com>
+ * @copyright    2009 Red Tree Systems, LLC
+ * @license      MPL 1.1
+ * @version      2.0
+ */
+
+/**
+ * Implemnts a data table
+ *   CREATE TABLE DBO_data(
+ *       DBO_id INTEGER NOT NULL,
+ *       `key` VARCHAR(255) NOT NULL,
+ *       `type` ENUM ('string', 'json'),
+ *       value TEXT NOT NULL,
+ *       PRIMARY KEY (DBO_id, `key`),
+ *       INDEX(DBO_id),
+ *       INDEX(`key`)
+ *   );
+ */
+class CMSDBObjectDataTable
+{
+    public static $CustomSQL = array(
+        'datatable_keys' =>
+            'SELECT `key` FROM {table}_data WHERE {key}=? ORDER BY `key`',
+        'datatable_has' =>
+            'SELECT `key` FROM {table}_data WHERE {key}=? AND `key`=?',
+        'datatable_get' =>
+            'SELECT `type`, value {table}_data WHERE {key}=? AND `key`=?',
+        'datatable_set' =>
+            'REPLACE INTO {table}_data ({key}, `key`, `type`, value) VALUES (?, ?, ?, ?)',
+        'datatable_clear' =>
+            'DELETE FROM {table}_data WHERE {key}=? AND `key`=?',
+        'datatable_clear_all' =>
+            'DELETE FROM {table}_data WHERE {key}=?'
+    );
+
+    protected $dbo;
+
+    public function __construct(CMSDBObject $dbo)
+    {
+        $this->dbo = $dbo;
+    }
+
+    /**
+     * @return array of strings
+     */
+    public function keys()
+    {
+        if (isset($this->fake)) {
+            return array_keys($this->fake);
+        } else {
+            assert(isset($this->dbo->id));
+
+            global $database;
+            $sql = $this->dbo->meta()->getCustomSQL('datatable_keys');
+            $sth = $database->executef($sql, $this->dbo->id);
+            $key = null;
+            $sth->bindColumn(1, $key);
+            $r = array();
+            while ($sth->fetch) {
+                array_push($r, $key);
+            }
+            $database->free();
+            return $r;
+        }
+    }
+
+    /**
+     * @param key string
+     * @return bool
+     */
+    public function has($key)
+    {
+        if (isset($this->fake)) {
+            return array_key_exists($key, $this->fake);
+        } else {
+            assert(is_string($key));
+            assert(isset($this->dbo->id));
+
+            global $database;
+            $sql = $this->dbo->meta()->getCustomSQL('datatable_has');
+            $database->executef($sql, $this->dbo->id, $key);
+            $r = (bool) $database->count() > 0;
+            $database->free();
+            return $r;
+        }
+    }
+
+    /**
+     * @param key string
+     * @return mixed
+     */
+    public function get($key)
+    {
+        if (isset($this->fake)) {
+            if (array_key_exists($key, $this->fake)) {
+                return $this->fake[$key];
+            } else {
+                return null;
+            }
+        } else {
+            assert(is_string($key));
+            assert(isset($this->dbo->id));
+
+            global $database;
+            $sql = $this->dbo->meta()->getCustomSQL('datatable_get');
+            $sth = $database->executef($sql, $this->dbo->id, $key);
+            $r = null;
+            if ($database->count() > 0) {
+                $r = $sth->fetch(PDO::FETCH_NUM);
+                if ($r[0] == 'json') {
+                    $r = json_decode($r[1]);
+                } else {
+                    $r = $r[1];
+                }
+            }
+            $database->free();
+            return $r;
+        }
+    }
+
+    /**
+     * @param key string
+     * @param value mixed
+     * @return void
+     */
+    public function set($key, $value)
+    {
+        if (isset($this->fake)) {
+            $this->fake[$key] = $value;
+        } else {
+            assert(is_string($key));
+            assert(isset($this->dbo->id));
+            if (! isset($value)) {
+                $this->clear($key);
+            }
+
+            $type = 'string';
+            if (is_object($value) || is_array($value)) {
+                $type = 'json';
+                $value = json_encode($value);
+            } elseif (! is_string($value)) {
+                throw new InvalidArgumentException(
+                    "don't know how to handle that type of value"
+                );
+            }
+
+            global $database;
+            $sql = $this->dbo->meta()->getCustomSQL('datatable_set');
+            $database->executef($sql, $this->dbo->id, $key, $type, $value);
+            $database->free();
+            $this->dbo->setModified();
+        }
+    }
+
+    public function setArray($data)
+    {
+        if (isset($this->fake)) {
+            $this->fake = $data;
+        } else {
+            assert(is_array($data));
+            assert(isset($this->dbo->id));
+            if (! count($data)) {
+                return;
+            }
+
+            $d = array();
+            foreach ($data as $value) {
+                $type = 'string';
+                if (is_object($value) || is_array($value)) {
+                    $type = 'json';
+                    $value = json_encode($value);
+                } elseif (! is_string($value)) {
+                    throw new InvalidArgumentException(
+                        "don't know how to handle that type of value"
+                    );
+                }
+                array_push($d, $type, $value);
+            }
+
+            global $database;
+            $database->transaction();
+            try {
+                $sql = $this->dbo->meta()->getCustomSQL('datatable_set');
+                $sth = $database->prepare($sql);
+                for ($i=0; $i<count($d); $i+=2) {
+                    $sth->execute($this->dbo->id, $key, $d[$i], $d[$i+1]);
+                }
+                $database->free();
+            } catch (Exception $e) {
+                $database->rollback();
+                throw $e;
+            }
+            $database->commit();
+            $this->dbo->setModified();
+        }
+    }
+
+    public function clear($key)
+    {
+        if (isset($this->fake)) {
+            if (array_key_exists($key, $this->fake)) {
+                unset($this->fake[$key]);
+            }
+        } else {
+            assert(is_string($key));
+            assert(isset($this->dbo->id));
+
+            global $database;
+            $sql = $this->dbo->meta()->getCustomSQL('datatable_clear');
+            $database->executef($sql, $this->dbo->id, $key);
+            $database->free();
+            $this->dbo->setModified();
+        }
+    }
+
+    public function clearAll()
+    {
+        if (isset($this->fake)) {
+            $this->fake = array();
+        } else {
+            assert(isset($this->dbo->id));
+
+            global $database;
+            $sql = $this->dbo->meta()->getCustomSQL('datatable_clear_all');
+            $database->executef($sql, $this->dbo->id);
+            $database->free();
+            $this->dbo->setModified();
+        }
+    }
+
+    public function serialize()
+    {
+        if (isset($this->fake)) {
+            return $this->fake;
+        } else {
+            assert(isset($this->dbo->id));
+            $dboid = $this->dbo->id;
+
+            $data = array();
+            global $database;
+            $sql = $this->dbo->meta()->getCustomSQL('datatable_keys');
+            $keys = $database->executef($sql, $dboid);
+            if ($database->count() > 0) {
+                $key = null;
+                $sql = $this->dbo->meta()->getCustomSQL('datatable_get');
+                $get = $database->prepare($sql);
+                $keys->bindColumn(1, $key);
+                $r = array();
+                while ($keys->fetch) {
+                    $get->execute($dboid, $key);
+                    $r = $get->fetch(PDO::FETCH_NUM);
+                    if ($r[0] == 'json') {
+                        $r = json_decode($r[1]);
+                    } else {
+                        $r = $r[1];
+                    }
+                    $data[$key] = $r;
+                }
+                $database->free();
+            }
+            $database->free();
+            return $data;
+        }
+    }
+
+    public function unserialize($data, $save=true)
+    {
+        if ($save) {
+            $this->clearAll();
+            $this->setArray($data);
+        } else {
+            $this->fake = $data;
+        }
+    }
+}
+
+?>
